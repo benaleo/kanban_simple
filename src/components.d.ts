909d4da9ecbd551eb9fa1f0c8d2993e3048d9d64
BENALEO BAYU SATRIA <e250073@dti.co.id>
2025-05-06 09:54:29 +0700

    add specified component conf

---
+++ src/components.d.ts
@@ -0,0 +1,6 @@
+// This file provides TypeScript type definitions for Vue components
+declare module '@/components/DrawerDialog.vue' {
+  import { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
