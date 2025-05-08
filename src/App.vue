e558e0881503a14937b47aedf1345f023259b2bb
benaleo <benaleo.projects@gmail.com>
2025-05-08 13:33:43 +0700

    add script tracker

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
 import favicon from '@/assets/favicon/favicon.ico'
 import { Toaster } from 'vue-sonner'
+import { onMounted } from 'vue'
 
+// Tambahkan favicon
 document.head.insertAdjacentHTML('beforeend',
   `<link rel="icon" type="image/png" href="${favicon}" />`
 )
 
+// Fungsi untuk menambahkan tracker script
+onMounted(() => {
+  const script = document.createElement('script')
+  script.src = 'https://tracker.epicgoods.my.id/tracker.js?domain=kanban-simple-ashy.vercel.app&lisence=premium'
+  script.async = true
+  document.head.appendChild(script)
+})
 </script>
 
 <template>
-    <Toaster rich-colors />
-    <RouterView />
+  <Toaster rich-colors />
+  <RouterView />
 </template>
